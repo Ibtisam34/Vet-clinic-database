0262414a172f3bc9f26fcaafcc35eb6420491bc9
unknown <Riyanagwl@gmail.com>
2023-01-30 13:17:33 +0300

    update files

--- data.sql
+++ data.sql
@@ -1,5 +1,9 @@
 /* Populate database with sample data. */
-
-INSERT INTO animals (name) VALUES ('Luna');
-INSERT INTO animals (name) VALUES ('Daisy');
-INSERT INTO animals (name) VALUES ('Charlie');
+USE VET_CLINIC;
+INSERT INTO animals (name, date_of_birth, escape_attemps,neutered,weight_kg) 
+VALUES 
+('Agumon', '2020=02-03', 0,1,10.23);
+('Gabumon', '2020=02-03', 2,1,8.00);
+('Pikachu', '2020=02-03', 1,0,15.04);
+('Devimon', '2020=02-03', 5,1,11.00);
+ 
--- schema.sql
+++ schema.sql
@@ -1,5 +1,13 @@
 /* Database schema to keep the structure of entire database. */
+CREATE DATABASE VET_CLINIC:
+
+USE VET_CLINIC;
 
 CREATE TABLE animals (
-    name varchar(100)
+ id INT PRIMARY KEY AUTO_INCREMENT,
+ name VARCHAR(255),
+ date_of_birth DATE,
+ escape_attemps INT,
+ neutered BOOLEAN,
+ weight_kg DECIMAL(10,2)
 );
