89fc15476f9c0a285f35acb3009890c8b0990b16
unknown <Riyanagwl@gmail.com>
2023-01-31 12:58:54 +0300

    modified files

--- schema.sql
+++ schema.sql
@@ -11,3 +11,7 @@ CREATE TABLE animals (
  neutered BOOLEAN NOT NULL,
  weight_kg DECIMAL(5,2) NOT NULL
 );
+
+-- Add a column species of type string to the animals table
+ALTER TABLE animals
+ADD COLUMN species varchar(255);
